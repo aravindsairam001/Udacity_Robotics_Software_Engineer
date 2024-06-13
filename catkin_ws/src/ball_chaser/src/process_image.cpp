d077caf545806fa879801181f03ca3763a9c7885
Aravind <aravindsairam98@gmail.com>
2024-06-13 14:51:50 +0100

    Service call added

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -12,6 +12,10 @@ void drive_robot(float lin_x, float ang_z)
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
+    client.call(srv);
+    
+    if (!client.call(srv))
+        ROS_ERROR("Failed to call service safe_move");
 }
 
 // This callback function continuously executes and reads the image data
